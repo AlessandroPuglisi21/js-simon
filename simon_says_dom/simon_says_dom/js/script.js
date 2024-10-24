27b40c037f74bc58be384d8d3dd90ed508d69c68
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-24 16:45:43 +0200

    ADDED: Check Numbers and Refresh page

--- simon_says_dom/simon_says_dom/js/script.js
+++ simon_says_dom/simon_says_dom/js/script.js
@@ -13,6 +13,7 @@
 // - Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"
 
 let timeLeft = 5;
+let generatedNumbers = []; // Array per memorizzare i numeri generati
 
 //!CREAZIONE COUNTDOWN
 function countDown() {
@@ -33,15 +34,15 @@ function countDown() {
 countDown();
 
 //!GENERAZIONE CASUALE DI NUMERI
-
 const listNumbers = document.getElementById("numbers-list");
 
 function randomNumber() {
   //inizializzo un ciclo per generare 5 numeri random
   for (let i = 0; i < 5; i++) {
     let number = Math.floor(Math.random() * 50) + 1;
+    generatedNumbers.push(number); // Aggiungi il numero all'array
 
-    //creo gli elementi li che andranno nella lista ul dell'HTML 
+    //creo gli elementi li che andranno nella lista ul dell'HTML
     const listElement = document.createElement("li");
     listElement.textContent = number;
 
@@ -49,17 +50,52 @@ function randomNumber() {
     listNumbers.appendChild(listElement);
   }
 }
+
 //Stampo
 randomNumber();
 
-//! COMPARSA NUMERI RICORDATI 
-setTimeout(function (){
-    const element = document.getElementById("answers-form");
-    element.classList.remove('d-none')
-}, 6000)
+//! COMPARSA NUMERI RICORDATI
+setTimeout(function () {
+  const element = document.getElementById("answers-form");
+  element.classList.remove("d-none");
+}, 6000);
 
-//! SCOMPARSA NUMERI RANDOM 
-setTimeout(function(){
-    const element = document.getElementById("numbers-list");
-    element.classList.add('d-none')
-}, 6000)
+//! SCOMPARSA NUMERI RANDOM
+setTimeout(function () {
+  const element = document.getElementById("numbers-list");
+  element.classList.add("d-none");
+}, 6000);
+
+//! INDOVINA IL NUMERO
+document.getElementById("answers-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Previene il comportamento predefinito del modulo
+    checkNumbers();
+  });
+
+function checkNumbers() {
+  const inputs = document.querySelectorAll("#input-group input");
+  let userNumbers = Array.from(inputs).map((input) => parseInt(input.value)); // Ottieni i valori inseriti
+  let correctCount = 0;
+
+  // Controlla quanti numeri l'utente ha indovinato
+  userNumbers.forEach((num) => {
+    if (generatedNumbers.includes(num)) {
+      correctCount++;
+    }
+  });
+
+  // Mostra il messaggio di risultato
+  document.getElementById(
+    "message"
+  ).textContent = `Hai indovinato ${correctCount} numeri su 5!`;
+}
+
+//! FUNZIONE REFRESH DELLA PAGINA 
+
+function refreshPage(){
+    location.reload();
+}
+
+document.getElementById("refresh").addEventListener("click", function () {
+refreshPage()
+})
